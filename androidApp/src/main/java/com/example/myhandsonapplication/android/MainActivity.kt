1a952f4a6958fb532cc20804d3e81025d0d2dff1
Hiroyuki Kusu <kusu0806@gmail.com>
2024-01-30 17:51:17 +0900

    Initial commit

---
+++ androidApp/src/main/java/com/example/myhandsonapplication/android/MainActivity.kt
@@ -0,0 +1,55 @@
+package com.example.myhandsonapplication.android
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.*
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.myhandsonapplication.Greeting
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            MyApplicationTheme {
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colors.background,
+                ) {
+                    GreetingView(Greeting().greet())
+                }
+            }
+        }
+
+        val password = "it is password"
+    }
+}
+
+@Composable
+fun GreetingView(text: String) {
+    Text(text = text)
+}
+
+@Preview
+@Composable
+fun DefaultPreview() {
+    MyApplicationTheme {
+        GreetingView("Hello, Android!")
+    }
+}
+
+fun getName() {}
+
+fun hogera() {}
+
+
+fun Fuga() {}
+
+fun mogera() {}
+
+fun piyoraddd() {}
+
+fun piyora323() {}
